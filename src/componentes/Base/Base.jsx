109118a38e7e1fddf981555f7d0085255feb4b6d
YamilH <yamilhamui@gmail.com>
2023-02-22 18:41:07 -0300

    base muy base

--- src/componentes/Base/Base.jsx
+++ src/componentes/Base/Base.jsx
@@ -5,13 +5,34 @@ const Base = () => {
   return (
     <div className={classes.ContainerPage}>
       <div className={classes.ContainerBase}>
-        <div className={classes.TituloCiudad}>
-        <h2>Nombre de Ciudad</h2>
+        <div className={classes.Header}>
+          <div className={classes.TituloCiudad}>
+            <h2>Nombre de Ciudad</h2>
+          </div>
+        <div className={classes.TimeStamp}>
+             <h4>TimeStamp</h4>
         </div>
-        <div className={classes.GrupoPrincipal}></div>
-        <div className={classes.GrupoPrincipal}></div>
-        <div className={classes.GrupoPrincipal}></div>
-    </div>
+        </div>
+        <div className={classes.GrupoPrincipal}>
+          <div className={classes.Caritas}> <h4>CARITA</h4></div>
+          <div className={classes.Aqi}> <h4>57</h4></div>
+         <div className={classes.Referencia}> <h4>Moderada</h4></div>
+        </div>
+        <div className={classes.GrupoSecundario}>
+          <div className={classes.Temperatura}></div>
+            <div className={classes.IconoTemperatura}>png</div>
+            <div className={classes.ValorTemperatura}>27°</div>
+          <div className={classes.Humedad}>
+            <div className={classes.IconoHumedad}>png</div>
+            <div className={classes.ValorHumedad}>55%</div>
+          </div>
+         <div className={classes.Viento}>
+           <div className={classes.IconoViento}>png</div>
+            <div className={classes.ValorViento}>3.2km/h</div>
+         </div>
+        </div>
+
+      </div>
     </div>
   );
 }
