ad8483b68c31f619a791c57e8d86ee33a5ab311a
juligon <julianagon73@gmail.com>
2023-02-23 10:15:40 -0300

    request api

--- src/App.js
+++ src/App.js
@@ -13,6 +13,13 @@ function App() {
   
   }, []);
 
+  function fetchCityData(city, state, country){
+    fetch(`http://api.airvisual.com/v2/city?city=${city}&state=${state}&country=${country}&key=4e099eb4-585d-4aef-8d67-417d1b9de753`)
+  .then(response => response.json())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
+  }
+
   return (
     <div className="App">
               {
