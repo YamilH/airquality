9133e981b9320a2ca36e8ba577cbc4c1e09aa0ce
juligon <julianagon73@gmail.com>
2023-02-23 17:08:34 -0300

    completando request

--- src/App.js
+++ src/App.js
@@ -1,93 +1,86 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from "react";
 import axios from "axios";
-import Loader from './componentes/Loader/Loader.jsx';
-import Base from './componentes/Base/Base.jsx';
+import Loader from "./componentes/Loader/Loader.jsx";
+import Base from "./componentes/Base/Base.jsx";
 
-import iconocaritas from './media/ic-face-green.svg'
-import iconotemperatura from './media/humedad.svg';
-import iconohumedad from './media/humedad.svg';
-import iconoviento from './media/viento.svg';
+import iconocaritas from "./media/ic-face-green.svg";
+import iconotemperatura from "./media/humedad.svg";
+import iconohumedad from "./media/humedad.svg";
+import iconoviento from "./media/viento.svg";
 
-const {
-  API_URL,
-  API_KEY
-} = process.env;
-
+const { API_URL, API_KEY } = process.env;
 
 function App() {
+	const [loading, setLoading] = useState(false);
 
-  const [loading, setLoading] = useState(false);
+	useEffect(() => {
+		setLoading(true);
+		setTimeout(() => {
+			setLoading(false);
+		}, 6000);
+	}, []);
 
-  useEffect(() => {
-    setLoading(true);
-    setTimeout(() => {
-      setLoading(false)
-    }, 6000);
-  
+	const [baseState, setBaseState] = useState({
+		city: "",
+		timestamp: "",
+		aqi: "",
+		temperature: "",
+		humidity: "",
+		wind: "",
+	});
+
+  useEffect(()=>{
+    getCityData();
   }, []);
 
-  const [baseState, setBaseState] = useState({
-    city: "",
-    timestamp: "",
-    aqi: "",
-    temperature: "",
-    humidity: "",
-    wind: "",
-  })
+	// function fetchCityData(city, state, country){
+	//   fetch(`http://api.airvisual.com/v2/city?city=${city}&state=${state}&country=${country}&key=4e099eb4-585d-4aef-8d67-417d1b9de753`)
+	// .then((response) => response.json())
+	// .then(result => console.log(result))
+	// .catch(error => console.log('error', error));
+	// }
 
-  // function fetchCityData(city, state, country){
-  //   fetch(`http://api.airvisual.com/v2/city?city=${city}&state=${state}&country=${country}&key=4e099eb4-585d-4aef-8d67-417d1b9de753`)
-  // .then((response) => response.json())
-  // .then(result => console.log(result))
-  // .catch(error => console.log('error', error));
-  // }
+	const getCityData = () => {
+		try {
+			const apiUrl = axios.get(`${API_URL}/v2/nearest_city?key=${API_KEY}`);
+			apiUrl
+				.then((response) => response.json())
+				.then((data) => {
+					setBaseState({
+						city: data.city,
+						timestamp: data.current.pollution.ts,
+						aqi: data.current.pollution.aqius,
+						temperature: data.current.weather.tp,
+						humidity: data.current.weather.hu,
+						wind: data.current.weather.ws,
+					});
+				});
+		} catch (error) {
+			console.log(error);
+		}
+	};
 
-  const getCityData = () =>{
-    try {
-      const apiUrl = axios.get(
-        `${API_URL}/v2/nearest_city?key=${API_KEY}`
-      );
-        apiUrl.then((response) => response.json())
-              .then(data =>{
-                setBaseState({
-                  city: data.city,
-                  timestamp: data.current.pollution.ts,
-                  aqi: data.current.pollution.aqius,
-                  temperature: data.current.weather.tp,
-                  humidity: data.current.weather.hu,
-                  wind: data.current.weather.ws,
-                })
-              })
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  
-  return (
-    <div className="App">
-              {
-        loading ? (
-
-        <Loader />
-      ) : (
-
-        <Base
-          titulociudad={baseState.city}
-          timestamp={'1967 10-00-00'}
-          iconocaritas={iconocaritas}
-          aqi={'24°'}
-          referencia={'Buena como Juli'} 
-          iconotemperatura={iconotemperatura} 
-          valortemperatura={'23°'} 
-          iconohumedad={iconohumedad} 
-          valorhumedad={'23%'} 
-          iconoviento={iconoviento} 
-          valorviento={'10km/h'} 
-        
-        />
-      )}
-    </div>
-  );
+	return (
+		<div className="App">
+			{loading ? (
+				<Loader />
+			) : (
+				<Base
+					titulociudad={baseState.city}
+					timestamp={"1967 10-00-00"}
+					iconocaritas={iconocaritas}
+					aqi={"24°"}
+					referencia={"Buena como Juli"}
+					iconotemperatura={iconotemperatura}
+					valortemperatura={"23°"}
+					iconohumedad={iconohumedad}
+					valorhumedad={"23%"}
+					iconoviento={iconoviento}
+					valorviento={"10km/h"}
+				/>
+			)}
+		</div>
+	);
 }
 
 export default App;
